9235284b274f026c7d961882f725f49e47b147b0
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-05-02 23:38:11 +0900

    Add state variables for interval and time stamp

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,15 +9,36 @@ pragma solidity ^0.8.18;
  */
 
 contract Raffle {
+    error Raffle__NotEnoughEthSent();
     uint256 private immutable i_entranceFee; //make it immutable to save gas
+    uint256 private immutable i_interval; // @dev Duration of lottery in seconds
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
-    constructor(uint256 entranceFee) {
+    event EnteredRaffle(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        if (msg.value >= i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
 
-    function pickWinner() public {}
+    // Get random number
+    // Use random number to pick player
+    // Be called automatically
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+    }
 
     // Getter Function
     function getEntranceFee() external view returns (uint256) {
